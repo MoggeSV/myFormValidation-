fd9e70ba6908edca2a79cc0a7880fb9201528809
Morgan Svensson <morgan.svensson10@gmail.com>
2019-01-20 16:34:20 +0100

    submit och radio kvar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(function () {
+   'use strict';
 
     let formValid = {
         firstname: false,
@@ -9,222 +10,204 @@ $(function () {
         city: false,
         zip: false,
         birthyear: false,
-        occupation: true,
+        occupation: false,
         textarea: false
     };
 
-    $('form').submit(function (e) {
-    if (formValid.firstname == true && formValid.lastname == true && formValid.email == true && formValid.password == true && formValid.address == true && formValid.city == true && formValid.zip == true && formValid.birthyear == true && formValid.occupation == true && formValid.textarea == true) {
-        //window.location.href = 'registration.html';
-        alert("Registrering lyckades");
-        return;
-    } else {
-        $('#alertRow').addClass("alert alert-danger");
-        $('#alertText').show(100);
-        alert("registreringen misslyckades");
-        /*
-        if (formvValid.firstname == false) {
-            $("#inputFirstName").removeClass("is-valid").addClass("is-invalid");
-            e.preventDefault();
-        } else if (formValid.lastname == false) {
-            $("#inputLastName").removeClass("is-valid").addClass("is-invalid");
-            e.preventDefault();
-        } else if (formValid.email == false){
-            $("#inputEmail").removeClass("is-valid").addClass("is-invalid");
-            e.preventDefault();
-        } else if (formValid.password == false){
-            $("#inputPassword").removeClass("is-valid").addClass("is-invalid");
-            e.preventDefault();
-        } else if (formValid.address == false){
-            $("#inputAddress").removeClass("is-valid").addClass("is-invalid");
-            e.preventDefault();
-        } else if (formValid.city == false){
-            $("#inputCity").removeClass("is-valid").addClass("is-invalid");
-            e.preventDefault();
-        } else if (formValid.zip == false){
-            $("#inputZip").removeClass("is-valid").addClass("is-invalid");
-            e.preventDefault();
-        } else if (formValid.birthyear == false){
-            $("#inputAge").removeClass("is-valid").addClass("is-invalid");
-            e.preventDefault();
-        } else if (formValid.occupation == false){
-            $("#inputRadio").removeClass("is-valid").addClass("is-invalid");
-            e.preventDefault();
-        } else if (formValid.textarea == false){
-            $("#inputTextArea").removeClass("is-valid").addClass("is-invalid");
-            e.preventDefault();
-        }*/
-        
-    }
-    e.preventDefault();
+    //submit
+
+    $("#submit").click(function (event) {
+        console.log(formValid.occupation);
+        if (formValid.firstname == false || formValid.lastname == false || formValid.email == false || formValid.password == false || formvalid.address == false || formValid.city == false || formValid.zip == false || formValid.birthyear == false || formValid.occupation == false || formValid.textarea == false) {
+
+            event.preventDefault();
+            event.stopPropagation();
+            $('#alertRow').addClass("alert alert-danger");
+            $('#alertText').show();
+            
+        }
+            
     });
 
-    function isInvalid(input) {
-        
-    }
 
 
-    $('#inputAge').click(function () {
+    //functions
+
+    $('#inputAge').ready(function () {
         for (let i = 2019; i > 1919; i--) {
             $('#inputAge').append('<option class="ageOption">' + i + '</option>');
         }
     });
 
+    function isValid(element, validClass = "is-valid", invalidClass= "is-invalid") {
+        $(element).addClass(validClass);
+        $(element).removeClass(invalidClass);
+    }
 
-    $('#inputFirstName').on('change', function () {
-        let input = $(this);
+    function isInvalid(element, validClass = "is-valid", invalidClass= "is-invalid") {
+        $(element).addClass(invalidClass);
+        $(element).removeClass(validClass);
+    } 
 
-        if (input.val().length > 1) {
-            console.log('Fungerar');
-            $(this).removeClass("is-invalid").addClass("is-valid");
-            formValid.firstname = true;
-        } else {
-            console.log('Fungerar inte');
-            $(this).removeClass("is-valid").addClass("is-invalid");
+    //Text inputs
+
+    $("#inputFirstName").blur(function () {
+        if ($(this).val().length < 2) {
+            isInvalid(this);
             formValid.firstname = false;
-            
+        } else {
+            isValid(this);
+            formValid.firstname = true;
         }
-
     });
 
-    $('#inputLastName').on('change', function () {
-        let input = $(this);
-
-        if (input.val().length > 1) {
-            console.log('Fungerar');
-            $(this).removeClass("is-invalid").addClass("is-valid");
-            formValid.lastname = true;
-        } else {
-            console.log('Fungerar inte');
-            $(this).removeClass("is-valid").addClass("is-invalid");
+    $("#inputLastName").blur(function () {
+        if ($(this).val().length < 2) {
+            isInvalid(this);
             formValid.lastname = false;
-        }
-    });
-
-
-    $('#inputEmail').on('change', function () {
-        let input = $(this);
-
-        if (validateEmail(input.val())) {
-            input.removeClass('is-invalid').addClass('is-valid');
-            formValid.email = true;
-            console.log('Please enter a valid email address.');
         } else {
-            input.addClass('is-invalid');
-            formValid.email = false;
+            isValid(this);
+            formValid.lastname = true;
         }
-
-        return false;
     });
+
+    $("#inputAddress").blur(function () {
+        if ($(this).val().length < 2) {
+            isInvalid(this);
+            formValid.address = false;
+        } else {
+            isValid(this);
+            formValid.address = true;
+        }
+    });
+
+    $("#inputCity").blur(function () {
+        if ($(this).val().length < 2) {
+            isInvalid(this);
+            formValid.city = false;
+        } else {
+            isValid(this);
+            formValid.city = true;
+        }
+    });
+
+    $("#inputTextArea").blur(function () {
+        if ($(this).val().length < 20) {
+            isInvalid(this);
+            formValid.textarea = false;
+        } else {
+            isValid(this);
+            formValid.textarea = true;
+        }
+    });
+
+    //Email, password, postalnumber REGEX test
+
 
     function validateEmail(email) {
         let regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return regex.test(email);
     };
 
-    $('#inputPassword').on('change', function () {
-        let input = $(this);
-
-        if (validatePassword(input.val())) {
-            console.log('Fungerar');
-            $(this).removeClass("is-invalid").addClass("is-valid");
-            formValid.password = true;
-        } else {
-            console.log('Fungerar inte');
-            $(this).removeClass("is-valid").addClass("is-invalid");
-            formValid.password = false;
-        }
-    });
-
     function validatePassword(password) {
         let regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
         return regex.test(password);
     };
 
-
-    $('#inputAddress').on('change', function () {
-        let input = $(this);
-
-        if (input.val().length > 1) {
-            console.log('Fungerar');
-            $(this).removeClass("is-invalid").addClass("is-valid");
-            formValid.address = true;
-        } else {
-            console.log('Fungerar inte');
-            $(this).removeClass("is-valid").addClass("is-invalid");
-            formValid.address = false;
-        }
-    });
-
-    $('#inputCity').on('change', function () {
-        let input = $(this);
-
-        if (input.val().length > 1) {
-            console.log('Fungerar');
-            $(this).removeClass("is-invalid").addClass("is-valid");
-            formValid.city = true;
-        } else {
-            console.log('Fungerar inte');
-            $(this).removeClass("is-valid").addClass("is-invalid");
-            formValid.city = false;
-        }
-    });
-
-    $('#inputZip').on('change', function () {
-        let input = $(this);
-
-        if (input.val().length == 5 && validateZip(input.val())) {
-            console.log('Fungerar');
-            $(this).removeClass("is-invalid").addClass("is-valid");
-            formValid.zip = true;
-        } else {
-            console.log('Fungerar inte');
-            $(this).removeClass("is-valid").addClass("is-invalid");
-            formValid.zip = false;
-        }
-    });
-
     function validateZip(zip) {
-        let regex = /^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$/;
+        let regex = /^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]{5,}$/;
         return regex.test(zip);
     };
 
+    $('#inputEmail').blur(function () {
+
+        if (validateEmail($(this).val())) {
+            isValid(this);
+            formValid.email = true;
+        } else {
+            isInvalid(this);
+            formValid.email = false;
+        }
+    });
+
+    $('#inputPassword').blur(function () {
+
+        if (validatePassword($(this).val())) {
+            isValid(this);
+            formValid.password = true;
+        } else {
+            isInvalid(this);
+            formValid.password = false;
+        }
+    });
+
+    $('#inputZip').blur(function () {
+         if (validateZip($(this).val())) {
+                isValid(this);
+                formValid.zip = true;
+            } else {
+                isInvalid(this);
+                formValid.zip = false;
+            }
+    });
+
+    //Select validation
+
     $('#inputAge').on('change', function () {
         let age = $('#inputAge option:selected').val();
-        console.log(age);
+        
         
         if (age == "" || age == "Välj ditt födelseår") {
+            isInvalid(this);
             formValid.birthyear = false;
-            $(this).removeClass("is-valid").addClass("is-invalid");
         } else {
+            isValid(this);
             formValid.birthyear = true;
-            $(this).removeClass("is-invalid").addClass("is-valid");
         }
     });
 
-    /*$('#inputRadio').on('change', function () {
-       if ($('input[name=customRadio]:checked').length <= 0) {
-           
-       } 
-    });*/
-
-    
-
-    $('#inputTextArea').on('change', function () {
-        let input = $(this);
-
-        if (input.val().length > 20) {
-            console.log('Fungerar');
-            $(this).removeClass("is-invalid").addClass("is-valid");
-            formValid.textarea = true;
+    $("#inputAge").blur(function () {
+        let age = $('#inputAge option:selected').val();
+        if (age == "" || age == "Välj ditt födelseår") {
+            isInvalid(this);
+            formValid.birthyear = false;
         } else {
-            console.log('Fungerar inte');
-            $(this).removeClass("is-valid").addClass("is-invalid");
-            formValid.textarea = false;
+            isValid(this);
+            formValid.birthyear = true;
         }
     });
 
-    $('#inputCheck').on('click', function () {
+    //Radiobuttons
+
+    $('input[name=customRadio]').on('change', function () {
+        if ($('input[name=customRadio]:checked').length == 0) {
+            console.log($('input[name=customRadio]:checked').length);
+            isInvalid(($('input[name=customRadio]')));
+            formValid.occupation = false;
+        } else {
+            isValid(($('input[name=customRadio]')));
+            console.log($('input[name=customRadio]:checked').length);
+            formValid.occupation = true;
+        }
+     });
+
+     $('input[name=customRadio]').blur(function () {
+        if ($('input[name=customRadio]:checked').length == 0) {
+            console.log($('input[name=customRadio]:checked').length);
+            isInvalid(($('input[name=customRadio]')));
+            formValid.occupation = false;
+            console.log(formValid.occupation);
+        } else {
+            isValid(($('input[name=customRadio]')));
+            console.log($('input[name=customRadio]:checked').length);
+            formValid.occupation = true;
+            console.log(formValid.occupation);
+        }
+     });
+
+     //checkbox and submit button
+
+     $('#inputCheck').on('change', function () {
         if ($(this).is(':checked')) {
             $(this).removeClass("is-invalid").addClass("is-valid");
             $('#submit').prop("disabled", false);
@@ -237,6 +220,52 @@ $(function () {
 
 
 
-
-
+    /*
+    window.addEventListener("load", function() {
+        var forms = document.getElementsByClassName("needs-validation");
+        Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener("submit", function(event) {
+                if(form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add("was-validated");
+            }, false);
+        });
+    }, false);
+    */
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/temp.js
@@ -0,0 +1,243 @@
+$(function () {
+
+    let formValid = {
+        firstname: false,
+        lastname: false,
+        email: false,
+        password: false,
+        address: false,
+        city: false,
+        zip: false,
+        birthyear: false,
+        occupation: true,
+        textarea: false
+    };
+
+    $('form').submit(function (e) {
+    if (formValid.firstname == true && formValid.lastname == true && formValid.email == true && formValid.password == true && formValid.address == true && formValid.city == true && formValid.zip == true && formValid.birthyear == true && formValid.occupation == true && formValid.textarea == true) {
+        //window.location.href = 'registration.html';
+        alert("Registrering lyckades");
+        return;
+    } else {
+        $('#alertRow').addClass("alert alert-danger");
+        $('#alertText').show(100);
+        alert("registreringen misslyckades");
+        /*
+        if (formvValid.firstname == false) {
+            $("#inputFirstName").removeClass("is-valid").addClass("is-invalid");
+            e.preventDefault();
+        } else if (formValid.lastname == false) {
+            $("#inputLastName").removeClass("is-valid").addClass("is-invalid");
+            e.preventDefault();
+        } else if (formValid.email == false){
+            $("#inputEmail").removeClass("is-valid").addClass("is-invalid");
+            e.preventDefault();
+        } else if (formValid.password == false){
+            $("#inputPassword").removeClass("is-valid").addClass("is-invalid");
+            e.preventDefault();
+        } else if (formValid.address == false){
+            $("#inputAddress").removeClass("is-valid").addClass("is-invalid");
+            e.preventDefault();
+        } else if (formValid.city == false){
+            $("#inputCity").removeClass("is-valid").addClass("is-invalid");
+            e.preventDefault();
+        } else if (formValid.zip == false){
+            $("#inputZip").removeClass("is-valid").addClass("is-invalid");
+            e.preventDefault();
+        } else if (formValid.birthyear == false){
+            $("#inputAge").removeClass("is-valid").addClass("is-invalid");
+            e.preventDefault();
+        } else if (formValid.occupation == false){
+            $("#inputRadio").removeClass("is-valid").addClass("is-invalid");
+            e.preventDefault();
+        } else if (formValid.textarea == false){
+            $("#inputTextArea").removeClass("is-valid").addClass("is-invalid");
+            e.preventDefault();
+        }*/
+        
+    }
+    e.preventDefault();
+    });
+
+    function isInvalid(input) {
+        
+    }
+
+
+    $('#inputAge').click(function () {
+        for (let i = 2019; i > 1919; i--) {
+            $('#inputAge').append('<option class="ageOption">' + i + '</option>');
+        }
+    });
+
+
+    $('#inputFirstName').on('change', function () {
+        let input = $(this);
+
+        if (input.val().length > 1) {
+            console.log('Fungerar');
+            $(this).removeClass("is-invalid").addClass("is-valid");
+            formValid.firstname = true;
+        } else {
+            console.log('Fungerar inte');
+            $(this).removeClass("is-valid").addClass("is-invalid");
+            formValid.firstname = false;
+            
+        }
+
+    });
+
+    $('#inputLastName').on('change', function () {
+        let input = $(this);
+
+        if (input.val().length > 1) {
+            console.log('Fungerar');
+            $(this).removeClass("is-invalid").addClass("is-valid");
+            formValid.lastname = true;
+        } else {
+            console.log('Fungerar inte');
+            $(this).removeClass("is-valid").addClass("is-invalid");
+            formValid.lastname = false;
+        }
+    });
+
+
+    $('#inputEmail').on('change', function () {
+        let input = $(this);
+
+        if (validateEmail(input.val())) {
+            input.removeClass('is-invalid').addClass('is-valid');
+            formValid.email = true;
+            console.log('Please enter a valid email address.');
+        } else {
+            input.addClass('is-invalid');
+            formValid.email = false;
+        }
+
+        return false;
+    });
+
+    function validateEmail(email) {
+        let regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regex.test(email);
+    };
+
+    $('#inputPassword').on('change', function () {
+        let input = $(this);
+
+        if (validatePassword(input.val())) {
+            console.log('Fungerar');
+            $(this).removeClass("is-invalid").addClass("is-valid");
+            formValid.password = true;
+        } else {
+            console.log('Fungerar inte');
+            $(this).removeClass("is-valid").addClass("is-invalid");
+            formValid.password = false;
+        }
+    });
+
+    function validatePassword(password) {
+        let regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+        return regex.test(password);
+    };
+
+
+    $('#inputAddress').on('change', function () {
+        let input = $(this);
+
+        if (input.val().length > 1) {
+            console.log('Fungerar');
+            $(this).removeClass("is-invalid").addClass("is-valid");
+            formValid.address = true;
+        } else {
+            console.log('Fungerar inte');
+            $(this).removeClass("is-valid").addClass("is-invalid");
+            formValid.address = false;
+        }
+    });
+
+    $('#inputCity').on('change', function () {
+        let input = $(this);
+
+        if (input.val().length > 1) {
+            console.log('Fungerar');
+            $(this).removeClass("is-invalid").addClass("is-valid");
+            formValid.city = true;
+        } else {
+            console.log('Fungerar inte');
+            $(this).removeClass("is-valid").addClass("is-invalid");
+            formValid.city = false;
+        }
+    });
+
+    $('#inputZip').on('change', function () {
+        let input = $(this);
+
+        if (input.val().length == 5 && validateZip(input.val())) {
+            console.log('Fungerar');
+            $(this).removeClass("is-invalid").addClass("is-valid");
+            formValid.zip = true;
+        } else {
+            console.log('Fungerar inte');
+            $(this).removeClass("is-valid").addClass("is-invalid");
+            formValid.zip = false;
+        }
+    });
+
+    function validateZip(zip) {
+        let regex = /^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$/;
+        return regex.test(zip);
+    };
+
+    $('#inputAge').on('change', function () {
+        let age = $('#inputAge option:selected').val();
+        console.log(age);
+        
+        if (age == "" || age == "Välj ditt födelseår") {
+            formValid.birthyear = false;
+            $(this).removeClass("is-valid").addClass("is-invalid");
+        } else {
+            formValid.birthyear = true;
+            $(this).removeClass("is-invalid").addClass("is-valid");
+        }
+    });
+
+    /*$('#inputRadio').on('change', function () {
+       if ($('input[name=customRadio]:checked').length <= 0) {
+           
+       } 
+    });*/
+    
+
+    
+
+    $('#inputTextArea').on('change', function () {
+        let input = $(this);
+
+        if (input.val().length > 20) {
+            console.log('Fungerar');
+            $(this).removeClass("is-invalid").addClass("is-valid");
+            formValid.textarea = true;
+        } else {
+            console.log('Fungerar inte');
+            $(this).removeClass("is-valid").addClass("is-invalid");
+            formValid.textarea = false;
+        }
+    });
+
+    $('#inputCheck').on('click', function () {
+        if ($(this).is(':checked')) {
+            $(this).removeClass("is-invalid").addClass("is-valid");
+            $('#submit').prop("disabled", false);
+        } else {
+            $(this).removeClass("is-valid").addClass("is-invalid");
+            $('#submit').prop("disabled", true);
+        }
+
+    });
+
+
+
+
+
+});
